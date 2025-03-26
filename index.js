1753accd97678ff3856430e5585397fc1f98594e
Arnold <arnoldkorir201@gmail.com>
2025-03-26 11:14:41 +0300

    write a function that displays onclick button to changethe theme

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 // fetch data from the GitHub ApI
 const themeToggleBtn = document.getElementById("theme-toggle-btn");
+
+// Toggle Dark Mode
+themeToggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+});
