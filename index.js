a4a4ecfc444b667829f60c933cd6fea254d432a3
Arnold <arnoldkorir201@gmail.com>
2025-03-26 11:19:18 +0300

    create a search bar container button

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // fetch data from the GitHub ApI
+
+const searchBtn = document.getElementById("search-btn");
+const searchInput = document.getElementById("search-input");
 const themeToggleBtn = document.getElementById("theme-toggle-btn");
 
 // Toggle Dark Mode
