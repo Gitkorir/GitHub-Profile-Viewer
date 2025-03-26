bc76b7dc9ea292a7ea7e1b213a8385e63d0a752f
Arnold <arnoldkorir201@gmail.com>
2025-03-26 11:22:39 +0300

    create a function that fetches data from the gitHub api

--- index.js
+++ index.js
@@ -2,7 +2,25 @@
 
 const searchBtn = document.getElementById("search-btn");
 const searchInput = document.getElementById("search-input");
+const profileContainer = document.getElementById("profile-container");
 const themeToggleBtn = document.getElementById("theme-toggle-btn");
+
+// Fetch GitHub Profile Data
+async function fetchProfileData(username) {
+  try {
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    const data = await response.json();
+
+    if (data.message === "Not Found") {
+      throw new Error("User not found");
+    }
+
+    displayProfileData(data);
+  } catch (error) {
+    errorMessage.textContent = error.message;
+    profileContainer.innerHTML = "";
+  }
+}
 
 // Toggle Dark Mode
 themeToggleBtn.addEventListener("click", () => {
